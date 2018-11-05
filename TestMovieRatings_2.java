a371a3281e98e47de168f08b9359a44ab4546daf
Nate Devine <ndevine@conncoll.edu>
2018-11-04 20:53:41 -0500

    part 1 complete except for getter methods

--- TestMovieRatings_2.java
+++ TestMovieRatings_2.java
@@ -47,7 +47,7 @@ public class TestMovieRatings_2 {
 
 	public double findIDBasedAverage(String movieID, int minimumNumOfRaterPersons){
 		if (amountRatingsByMovie(movieID, listOfRaterPersons) < minimumNumOfRaterPersons){
-			System.out.println("Not enough rater persons to calculate the arithmetic mean");
+			//System.out.println("Not enough rater persons to calculate the arithmetic mean");
 			return -1.0;
 		}
 		else{
@@ -57,7 +57,7 @@ public class TestMovieRatings_2 {
 				if (rater.hasRatingValue(movieID)){
 					total += rater.getRatingValues(movieID);
 					tally += 1;
-					System.out.println(total);
+					//System.out.println(total);
 				}
 			}
 			return total / tally;
@@ -65,8 +65,21 @@ public class TestMovieRatings_2 {
 
 	}
 
-	public int findRatingsAverage(int minimumNumberOfRaterPersons){
+	public HashMap findRatingsAverage(int minimumNumberOfRaterPersons){
+		HashMap<String, Double> ratingsAverage = new HashMap<>();
+		for (MovieInformation movie : this.listOfMovies){
+			ratingsAverage.put(movie.getMovieID(), findIDBasedAverage(movie.getMovieID(), minimumNumberOfRaterPersons));
+		}
+		//System.out.println(ratingsAverage);
+		return ratingsAverage;
+	}
 
+	public String getMovieTitle(String movieID){
+		
+	}
+
+	public String getMovieID(String movieTitle){
+		
 	}
 
 public static void main(String[] args){
@@ -74,6 +87,7 @@ public static void main(String[] args){
 	System.out.println("Number of movies is: " + movieRatings2.findNumberOfMovies());
 	System.out.println("Number of rater persons is: " + movieRatings2.findNumberOfRaterPersons());
 	System.out.println(movieRatings2.findIDBasedAverage("68646", 5));
+	System.out.println(movieRatings2.findRatingsAverage(0));
 
 }
 
