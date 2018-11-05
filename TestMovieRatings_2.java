7c3ec45d47c4c9fd4d0dfca552685e64ed84bb73
Nate Devine <ndevine@conncoll.edu>
2018-11-04 21:35:39 -0500

    commented all of my bullshit code fml

--- TestMovieRatings_2.java
+++ TestMovieRatings_2.java
@@ -24,25 +24,30 @@ public class TestMovieRatings_2 {
 		this("data/Rated-Movies-for-Testing.csv", "data/Movie-Ratings-for-Testing.csv");
 	}
 
+	//returns the number of movies stored in the listOfMovies field
 	public int findNumberOfMovies(){
 		return listOfMovies.size();
 	}
 
+	//test method for findNumberOfMovies()
 	public void testFindNumberOfMovies(){
 		TestMovieRatings_2 movieRatings2 = new TestMovieRatings_2("data/Rated-Movies-for-Testing.csv", "data/Movie-Ratings-for-Testing.csv");
 		System.out.println("Number of movies is: " + movieRatings2.findNumberOfMovies());
 	}
 
+	//returns the number of rater persons stored in the listOfRaterPersons field
 	public int findNumberOfRaterPersons(){
 		return listOfRaterPersons.size();
 	}
 
+	//test method for findNumberOfRaterPersons()
 	public void testFindNumberOfRaterPersons(){
 		TestMovieRatings_2 movieRatings2 = new TestMovieRatings_2("data/Rated-Movies-for-Testing.csv", "data/Movie-Ratings-for-Testing.csv");
 		System.out.println("Number of rater persons is: " + movieRatings2.findNumberOfRaterPersons());
 	}
 
 	//Gets the amount of times a move has been rated
+	//This is a ported method from TestMovieRatings
     public int amountRatingsByMovie(String id, ArrayList<RaterPerson> raterData){
         int ratings = 0;
         for (RaterPerson rater : raterData){
@@ -51,6 +56,8 @@ public class TestMovieRatings_2 {
         return ratings;
     }
 
+    /*Calculates the mean of movie ratings for a given movie ID that has been rated by the
+    minimum number of rater persons and returns the result*/
 	public double findIDBasedAverage(String movieID, int minimumNumOfRaterPersons){
 		if (amountRatingsByMovie(movieID, listOfRaterPersons) < minimumNumOfRaterPersons){
 			//System.out.println("Not enough rater persons to calculate the arithmetic mean");
@@ -70,11 +77,15 @@ public class TestMovieRatings_2 {
 
 	}
 
+	//test method for findIDBasedAverage()
 	public void testFindIDBasedAverage(){
 		TestMovieRatings_2 movieRatings2 = new TestMovieRatings_2("data/Rated-Movies-for-Testing.csv", "data/Movie-Ratings-for-Testing.csv");
 		System.out.println(movieRatings2.findIDBasedAverage("68646", 5));
 	}
 
+
+	/*Calculates the mean of those movies rated by the minimum number of rater persons 
+	and returns a dictionary (hash map) containing the result*/
 	public HashMap findRatingsAverage(int minimumNumberOfRaterPersons){
 		HashMap<String, Double> ratingsAverage = new HashMap<>();
 		for (MovieInformation movie : this.listOfMovies){
@@ -83,11 +94,13 @@ public class TestMovieRatings_2 {
 		return ratingsAverage;
 	}
 
+	//test method for findRatingsAverage()
 	public void testFindRatingsAverage(){
 		TestMovieRatings_2 movieRatings2 = new TestMovieRatings_2("data/Rated-Movies-for-Testing.csv", "data/Movie-Ratings-for-Testing.csv");
 		System.out.println(movieRatings2.findRatingsAverage(0));
 	}
 
+	//Uses the movie ID to get the title
 	public String getMovieTitle(String movieID){
 		for (MovieInformation movie : this.listOfMovies){
 			if (movie.getMovieID().equals(movieID)){
@@ -98,11 +111,14 @@ public class TestMovieRatings_2 {
 		return null;
 	}
 
+	//test method for getMovieTitle()
 	public void testGetMovieTitle(){
 		TestMovieRatings_2 movieRatings2 = new TestMovieRatings_2("data/Rated-Movies-for-Testing.csv", "data/Movie-Ratings-for-Testing.csv");
 		System.out.println(movieRatings2.getMovieTitle("1798709"));
 	}
 
+
+	//uses the movie title to get the ID
 	public String getMovieID(String movieTitle){
 		for (MovieInformation movie : this.listOfMovies){
 			if (movie.getMovieTitle().equals(movieTitle)){
@@ -112,6 +128,7 @@ public class TestMovieRatings_2 {
 		return null;
 	}
 
+	//test method for getMovieID()
 	public void testGetMovieID(){
 		TestMovieRatings_2 movieRatings2 = new TestMovieRatings_2("data/Rated-Movies-for-Testing.csv", "data/Movie-Ratings-for-Testing.csv");
 		System.out.println(movieRatings2.getMovieID("Her"));
